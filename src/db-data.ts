7cb39be9965d56d834717b43bb4177a58f4b325e
yvgo <yvonne.goetzeler@gmail.com>
2023-07-19 12:07:48 +0200

    css-images

--- src/db-data.ts
+++ src/db-data.ts
@@ -1,17 +1,16 @@
 export const PROJECTS: any = [
 
-   /*  undefined, */
+    /*  undefined, */
 
     {
         id: 1,
-        title: 'World of Pokemons',
-        imgUrl: '../../assets/img/project-poke.jpg',
-        //imgUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-ngrx-course.png',
-        longDescription: 'Based on the PokéApi a simple library that provides an catalogues pokemon information.',
-        keywords: 'JavaScript | API | HTML | CSS',
+        title: 'El Pollo Loco',
+        imgUrl: '../../assets/img/project-epl2.jpg',
+        longDescription: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins and bottles to fight against the Big Chicken called Endboss. ',
+        keywords: 'JavaScript | OOP | HTML | CSS',
         category: 'JavaScript',
-        webLink: 'https://yvonne-goetzeler.developerakademie.net/pokemon/',
-        gitLink: 'https://github.com/yvgo/js-pokemonWold',
+        webLink: 'https://yvonne-goetzeler.developerakademie.net/elpolloloco/',
+        gitLink: 'https://github.com/yvgo/js-elPoloLoco',
     },
     {
         id: 2,
@@ -25,25 +24,26 @@ export const PROJECTS: any = [
     },
     {
         id: 3,
-        title: 'El Pollo Loco',
-        imgUrl: '../../assets/img/project-epl.jpg',
-        longDescription: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins and bottles to fight against the Big Chicken called Endboss. ',
-        keywords: 'JavaScript | OOP | HTML | CSS',
+        title: 'World of Pokemons',
+        imgUrl: '../../assets/img/poke.jpg',
+        //imgUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-ngrx-course.png',
+        longDescription: 'Based on the PokéApi a simple library that provides an catalogues pokemon information.',
+        keywords: 'JavaScript | API | HTML | CSS',
         category: 'JavaScript',
-        webLink: 'https://yvonne-goetzeler.developerakademie.net/elpolloloco/',
-        gitLink: 'https://github.com/yvgo/js-elPoloLoco',
+        webLink: 'https://yvonne-goetzeler.developerakademie.net/pokemon/',
+        gitLink: 'https://github.com/yvgo/js-pokemonWold',
     },
     {
         id: 4,
         title: 'Portfolio',
-        imgUrl: '../../assets/img/project-poke.jpg',
+        imgUrl: '../../assets/img/portfolio-yg.jpg',
         longDescription: 'My Portfolio Page.',
         keywords: 'Angular | TypeScript | HTML | SCSS',
         category: 'Angular',
         webLink: '',
         gitLink: 'https://github.com/yvgo/ng-portfolioYvonneGoetzeler',
 
-        
+
     }
 
 ];
